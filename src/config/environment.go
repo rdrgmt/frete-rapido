7c47ed7d953f3495eb6b014b1ee91ef6eaf14a0a
matheus <rdrgmt@proton.me>
2024-07-19 04:26:46 -0300

    CHORE: create config file

---
+++ src/config/environment.go
@@ -0,0 +1,19 @@
+package config
+
+const (
+	// Database -
+	Database = "freterapido"
+	// Collection -
+	Collection = "quotes"
+)
+
+const (
+	// RegisteredNumber -
+	RegisteredNumber = "25438296000158"
+	// Token -
+	Token = "1d52a9b6b78cf07b08586152459a5c90"
+	// PlatformCode -
+	PlatformCode = "5AKVkHqCn"
+	// Path
+	Path = "https://sp.freterapido.com/api/v3/quote/simulate"
+)
